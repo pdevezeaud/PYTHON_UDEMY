e3cf52a95c2d9eefb566860ed8643165ecbe8f52
DEVEZEAUD Philippe <pdevezeaud@yahoo.fr>
2023-10-25 18:32:15 +0200

    Nombre de points

--- Jeu_de_maths/main.py
+++ Jeu_de_maths/main.py
@@ -1,27 +1,39 @@
 import random
 #NOMBRE_MAGIQUE = random.randint(NOMBRE_MIN,NOMBRE_MAX)
 
-NOMBRE_MIN = 1
-NOMBRE_MAX = 10
-NB_QUESTION = 4
 
 def poser_question():
     operateur_A = random.randint(NOMBRE_MIN,NOMBRE_MAX)
     operateur_B = random.randint(NOMBRE_MIN,NOMBRE_MAX)
-    #addition = operateur_A + operateur_B
+    addition = operateur_A + operateur_B
     question = input(f" qu'elle le resultat de {operateur_A} + {operateur_B}\n")
     reponse_addition = int(question)
 
     if reponse_addition == operateur_A + operateur_B:
-        print(f"Bonne réponse le resultat est bien {reponse_addition}")
-        
-    else:
-        print(f"Mauvaise réponse, la bonne réponse est {reponse_addition}")
-      
-        
-   
+        return True
     
+    else:
+        return False
+        
+        
+        
+
+NOMBRE_POINTS = 0
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NB_QUESTION = 4
+   
 for i in range(0,NB_QUESTION):
     print(F"question N° {i+1} sur {NB_QUESTION}")
-    poser_question()
+    if poser_question() == True:
+        print("Réponse correcte")
+        NOMBRE_POINTS += 1
+    else:
+        print("Réponse incorrect")
+        
+    print()
+        
+    
+
+print(f"votre note est de {NOMBRE_POINTS}/{NB_QUESTION}")
 
