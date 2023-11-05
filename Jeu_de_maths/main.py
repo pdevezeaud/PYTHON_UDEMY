a3cdfd8885170361a03d87771b052c340c922af5
DEVEZEAUD Philippe <pdevezeaud@yahoo.fr>
2023-11-05 14:14:33 +0100

    Add files via upload
    ajout du if en condition de choix

--- Jeu_de_maths/main.py
+++ Jeu_de_maths/main.py
@@ -5,35 +5,41 @@ import random
 def poser_question():
     operateur_A = random.randint(NOMBRE_MIN,NOMBRE_MAX)
     operateur_B = random.randint(NOMBRE_MIN,NOMBRE_MAX)
-    addition = operateur_A + operateur_B
-    question = input(f" qu'elle le resultat de {operateur_A} + {operateur_B}\n")
-    reponse_addition = int(question)
-
-    if reponse_addition == operateur_A + operateur_B:
-        return True
-    
+    operande_string = random.randint(0,1)
+    if operande_string == 0:
+        operande_string = "+"
     else:
-        return False
-        
-        
-        
+        operande_string = "*"
+    print(operande_string)
+
+    question = input(f" qu'elle le resultat de {operateur_A} {operande_string} {operateur_B}\n")
+    reponse_addition = int(question)
+    calcul = operateur_A + operateur_B
+    if operande_string == 0:
+        calcul = operateur_A + operateur_B
+    elif operande_string == 1:
+        calcul = operateur_A * operateur_B
+    else:
+        reponse_addition == calcul
+        return True
+
+    return False
+
 
 NOMBRE_POINTS = 0
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
 NB_QUESTION = 4
-   
-for i in range(0,NB_QUESTION):
-    print(F"question N° {i+1} sur {NB_QUESTION}")
+
+for i in range(0, NB_QUESTION):
+    print(F"question N° {i + 1} sur {NB_QUESTION}")
     if poser_question() == True:
         print("Réponse correcte")
         NOMBRE_POINTS += 1
     else:
         print("Réponse incorrect")
-        
+
     print()
-        
-    
 
 print(f"votre note est de {NOMBRE_POINTS}/{NB_QUESTION}")
 
@@ -47,4 +53,6 @@ elif NOMBRE_POINTS > NB_QUESTION:
     print("pas mal")
 else:
     print("Tu feras mieux la prochaine fois")
+
+
 
